9318a8b119d918bc571e6b2515cdaeb282c64ab6
Keshari Nandan Jha <kesharinandanjha15@gmail.com>
2025-02-23 01:11:37 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def download(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000 for local testing
+    app.run(host="0.0.0.0", port=port, debug=True)
